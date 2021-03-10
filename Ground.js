e07a862d6846f630dcf5caaebbaec570b7efda58
VP06062007 <74875145+VP06062007@users.noreply.github.com>
2021-03-10 18:21:13 +0000

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,14 @@
+class Ground{
+    var options = {
+        isStatic:true,
+        friction:0.4,
+    }
+    this.body = Bodies.rectangale(500,625,1200,20,options);
+    World.add(world, this.body);
+}
+display(){
+    
+    rectMode(CENTER);
+    fill("silver");
+    rect(500,625,1200,20);
+}
